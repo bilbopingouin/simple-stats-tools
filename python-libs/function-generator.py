b0ea28fa0d560c897e05583fbfa17ec0ee46e063
bilbopingouin <bilbopingouin@gmail.com>
2018-09-19 09:01:08 +0200

    Adding sine function generator | Test script of function_generator

--- python-libs/function-generator.py
+++ python-libs/function-generator.py
@@ -1,10 +1,12 @@
 # This class can generate various functions.
 
-class function_polynomial:
-    active = False
+import math
 
+#-----------------------------------------------------
+
+class function_polynomial:
     def __init__(self):
-        self.active = True
+        self.active = False
 
     def init(self,parameters):
         self.parameters = parameters.split(':')
@@ -32,14 +34,48 @@ class function_polynomial:
             print('ERR: polynomial function: error while converting the parameters')
             return 0
 
+        self.active = True
         return 1
 
     def get_value(self,x,y):
+        if not self.active:
+            return float('nan')
+
         s = 0
         for n in range(self.order+1):
             s = s*x + self.par_float[n]
         return s
         
+class function_sine:
+    def __init__(self):
+        self.active = False
+
+    def init(self,parameters):
+        self.parameters = parameters.split(':')
+
+        if len(self.parameters) < 3:
+            print('ERR: Sine function parameters not sufficient:',parameters)
+            return 0
+
+        try:
+            self.amplitude = float(self.parameters[0])
+            self.frequency = float(self.parameters[1])
+            self.phase     = float(self.parameters[2])
+        except:
+            print('ERR: Sine function parameters type not as expected:',parameters)
+            return 0
+
+        self.active = True
+        return 1
+
+    def get_value(self,x,y):
+        if not self.active:
+            return float('nan')
+
+        return self.amplitude*math.sin(2*math.pi*self.frequency*(x-self.phase))
+
+
+#-----------------------------------------------------
 
 class function_generator:
     skip = True
@@ -50,6 +86,8 @@ class function_generator:
     def init(self,function,parameters):
         if function == 'polynomial':
             self.function = function_polynomial()
+        elif function == 'sine':
+            self.function = function_sine()
         else:
             print('ERR: unknown function:',self.function)
             return 0
@@ -68,12 +106,18 @@ class function_generator:
 
 # Test unit
 if __name__ == '__main__':
-    import math
+    xbasis = 0
 
-    F = function_generator('polynomial','1:0.1:5')
+    functions = ['polynomial','polynomial','sine']
+    parameters= ['1:0.1:5','2:0.002:-0.3:1','5:0.1:0.5']
 
-    for n in range(100):
-        y = F.get_function_value(n,1)
-        if not math.isnan(y):
-            print(n,y)
+    for n in range(len(functions)):
+        F = function_generator(functions[n],parameters[n])
+
+        for n in range(100):
+            y = F.get_function_value(n,1)
+            if not math.isnan(y):
+                print(n+xbasis*100,y)
+
+        xbasis += 1
 
